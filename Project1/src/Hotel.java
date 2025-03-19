81cc757edf9081923db76fb6665dc3b3c1d10102
goeg1 <gcollier229@gmail.com>
2025-03-20 12:27:21 +1300

    First draft - Hotels Class

---
+++ Project1/src/Hotel.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author George
+ */
+public class Hotel {
+      String name;
+      String location;
+      int roomsAvail;
+      
+      public Hotel(String n,String l,int rooms){
+          this.name = n;
+          this.location = l;
+          this.roomsAvail = rooms;
+      }
+      
+      //Print out list of hotels
+        
+}
