2e6dd217371aa708266de3e7884f2e7eec992b0d
George <gcollier229@gmail.com>
2025-04-01 11:53:01 +1300

    Updating Menu

--- Project1/src/HotelManager.java
+++ Project1/src/HotelManager.java
@@ -19,55 +19,10 @@ import java.util.logging.Logger;
 public class HotelManager {
     public static void main(String[] args) {
         
-        //Read hotels and store into HashMap
-        System.out.println("\nTesting Hotel Locations:\n");
-        HashMap<String, Hotel> hotels = readHotels();
-        //Test accessing information from hotel objects stored in the hashmap
-        System.out.println(hotels.get("SkyCity").getLocation());
+        /*
+        Class where command line interface will run from
+        Call all the seperate methods from here, user input from here
         
-        //Print all names of hotels, because hashmap key is hotel name
-        for(String key : hotels.keySet()){
-            System.out.println(key);
-        }
-        
+        */
     }
-    
-    public static HashMap readHotels() {
-        // Read File Hotels.txt stored in the resources folder
-        // Splits data by each line and splits line by character "-"
-        // Prints data for each hotel from file, currently storing the data in a hashMap
-        HashMap<String, Hotel> hotels = new HashMap<>();
-        try {
-            FileReader fr = new FileReader("./resources/Hotels.txt");
-            BufferedReader inStream = new BufferedReader(fr);
-            String line = null;
-            while ((line = inStream.readLine()) != null) {
-                String str[] = line.split("-");
-                //Get variables from string array if str split properly
-                if (str.length == 3) {
-                    String name = str[0];
-                    String location = str[1];
-                    //Convert string to integer for rooms availabe
-                    int available = Integer.parseInt(str[2]);
-                    //Create new hotel object with read data to store a list
-                    Hotel h = new Hotel(name, location, available);
-                    //Add hotel object to hashmap where the key is the name, and the value is the hotel object
-                    hotels.put(name, h);
-                }
-                /* test string splitting
-                System.out.print("Hotel: " + str[0]);
-                System.out.print(" Location: " + str[1]);
-                System.out.print(" Rooms Available: " + str[2] + "\n");
-                */
-                
-            }
-            inStream.close();
-        } catch (FileNotFoundException e) {
-            Logger.getLogger(HotelManager.class.getName()).log(Level.SEVERE, null, e);
-        } catch (IOException e) {
-            System.out.println("IO Exception");
-        }
-        return hotels;
-    }
-    
 }
