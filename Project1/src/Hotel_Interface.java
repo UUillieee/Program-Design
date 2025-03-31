1b6da4ecf49833a54683a3d9c4d603ea59d3a321
George <gcollier229@gmail.com>
2025-04-01 11:28:27 +1300

    Interface

--- Project1/src/Hotel_Interface.java
+++ Project1/src/Hotel_Interface.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,72 +11,48 @@ import java.util.logging.Logger;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 /**
  *
- * @author George
+ * @author gcoll
  */
-public class Menu {
-
+public class Hotel_Interface {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-
-        //Fix this class before running
-        //CollectInfo collectInfo = new CollectInfo();
         
-        
-        /*
-        
-                    Testing Functions 
-                    Can Delete After 
-                    Need to Put it into its own method 
-                    e.g ListsRooms, List Hotels
-        
-        */
-         
-        
-
-        //Test Rooms
-        System.out.println("\nTesting Room Names:\n");
-           HashMap<String, Room> rooms = readRooms();
+        //Read hotels and store into HashMap
+        System.out.println("\nTesting Hotel Locations:\n");
+        HashMap<String, Hotel> hotels = readHotels();
         //Test accessing information from hotel objects stored in the hashmap
-        //System.out.println(rooms.get("Penthouse").getType());
+        System.out.println(hotels.get("SkyCity").getLocation());
         
         //Print all names of hotels, because hashmap key is hotel name
-        for(String key : rooms.keySet()){
+        for(String key : hotels.keySet()){
             System.out.println(key);
         }
         
-        
     }
-
     
-    
-    
-    
-     public static HashMap readRooms() {
-       //Adapt the readHotels function to read the different room types
-       
-        HashMap<String, Room> rooms = new HashMap<>();
+    public static HashMap readHotels() {
+        // Read File Hotels.txt stored in the resources folder
+        // Splits data by each line and splits line by character "-"
+        // Prints data for each hotel from file, currently storing the data in a hashMap
+        HashMap<String, Hotel> hotels = new HashMap<>();
         try {
-            FileReader fr = new FileReader("./resources/Rooms.txt");
+            FileReader fr = new FileReader("./resources/Hotels.txt");
             BufferedReader inStream = new BufferedReader(fr);
             String line = null;
-            int roomCount = 0;
             while ((line = inStream.readLine()) != null) {
                 String str[] = line.split("-");
                 //Get variables from string array if str split properly
-                if (str.length <= 4) {
-                    int idNumber = roomCount;
-                    roomCount++;
-                    String type = str[0];
-                    int price = Integer.parseInt(str[1]);
+                if (str.length <= 3) {
+                    String name = str[0];
+                    String location = str[1];
                     //Convert string to integer for rooms availabe
-                    boolean available = Boolean.parseBoolean(str[2]);
-                    int maxGuests = Integer.parseInt(str[3]);
-                    //Create new room object with read data to store a list
-                    Room r = new Room(idNumber,type,price,available,maxGuests);
+                    int available = Integer.parseInt(str[2]);
+                    //Create new hotel object with read data to store a list
+                    Hotel h = new Hotel(name, location, available);
                     //Add hotel object to hashmap where the key is the name, and the value is the hotel object
-                    rooms.put(type, r);
+                    hotels.put(name, h);
                 }
                 /* test string splitting
                 System.out.print("Hotel: " + str[0]);
@@ -92,7 +67,7 @@ public class Menu {
         } catch (IOException e) {
             System.out.println("IO Exception");
         }
-        return rooms;
+        return hotels;
     }
-     
+    
 }
